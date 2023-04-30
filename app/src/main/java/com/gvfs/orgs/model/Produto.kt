e7331012846e5a7a3ca1f37eb387d93268bcb616
GuilhermeVFSousa <guilhermevfsousa@gmail.com>
2023-04-30 19:15:19 -0300

    Adjusts

---
+++ app/src/main/java/com/gvfs/orgs/model/Produto.kt
@@ -0,0 +1,9 @@
+package com.gvfs.orgs.model
+
+import java.math.BigDecimal
+
+class Produto(
+    val nome: String,
+    val descricao: String,
+    val valor: BigDecimal
+)
---
+++ app/src/main/java/com/gvfs/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -0,0 +1,12 @@
+package com.gvfs.orgs.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gvfs.orgs.R
+
+class FormularioProdutoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_formulario_produto)
+    }
+}
